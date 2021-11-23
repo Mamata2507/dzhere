b0d99112ab2be22fa44baeb424d2a34e48234c7a
ryeongeunkim <ryeongeun.gim@gmail.com>
2021-11-23 14:13:22 +0900

    21.11.23(화) 사이드바 완성

--- dzhere-admin-front/src/components/common/CustomSidebarMenu.js
+++ dzhere-admin-front/src/components/common/CustomSidebarMenu.js
@@ -1,7 +1,7 @@
 // Custom Navigation Drawer / Sidebar with Image and Icon in Menu Options
 // https://aboutreact.com/custom-navigation-drawer-sidebar-with-image-and-icon-in-menu-options/
 import image from '../../../assets/logo.png';
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   View,
@@ -16,18 +16,110 @@ import {
   DrawerItemList,
   DrawerItem,
 } from '@react-navigation/drawer';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const CustomSidebarMenu = (props) => {
+  const [pressClass, setPressClass] = useState(false)
+  const onPressClass = () => {
+    setPressClass(!pressClass);
+  }
+  const [pressStudent, setPressStudent] = useState(false)
+  const onPressStudent = () => {
+    setPressStudent(!pressStudent);
+  }
+  const [pressTeacher, setPressTeacher] = useState(false)
+  const onPressTeacher = () => {
+    setPressTeacher(!pressTeacher);
+  }
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor:'#40cae9' }}>
       {/*Top Large Image */}
       <Image
         source={image}
-        style={styles.sideMenuProfileIcon}
+        style={styles.headerImage}
       />
-      <DrawerContentScrollView {...props}>
-        <DrawerItemList {...props} />
+      <DrawerContentScrollView {...props} >
+        {/* <DrawerItemList {...props} /> */}
+      <View>
+        <TouchableOpacity onPress={onPressClass}>
+          <Text style={styles.title}>강의 및 장소</Text>
+        </TouchableOpacity>
+      </View>
+      {pressClass === true ? 
+      <>
+        <DrawerItem
+          label="▪️ 강의 목록"
+          onPress={()=>{
+            props.navigation.navigate('ClassList')
+          }}
+        />
+        <DrawerItem 
+          label="▪️ 강의 관리"
+          onPress={()=>{
+            props.navigation.navigate('ClassManage')
+          }}
+        />
+        <DrawerItem 
+          label="▪️ 강의 장소 관리"
+          onPress={()=>{
+            props.navigation.navigate('ClassLocation')
+          }}
+        />
+        <DrawerItem 
+          label="▪️ 외부 장소 관리"
+          onPress={()=>{
+            props.navigation.navigate('ClassExternal')
+          }}
+        /></>
+      :<></>}
+      <View>
+        <TouchableOpacity onPress={onPressStudent}>
+          <Text style={styles.title}>수강생</Text>
+        </TouchableOpacity>
+      </View>
+      {pressStudent === true ? 
+      <>
+        <DrawerItem
+          label="▪️ 수강생 정보 관리"
+          onPress={()=>{
+            props.navigation.navigate('StudentManage')
+          }}
+        />
+        <DrawerItem 
+          label="▪️ 수강생별 출결 현황"
+          onPress={()=>{
+            props.navigation.navigate('StudentAttend')
+          }}
+        />
+        <DrawerItem 
+          label="▪️ 강의별 출결 현황(수강생)"
+          onPress={()=>{
+            props.navigation.navigate('StudentAttendClass')
+          }}
+        /></>
+      :<></>}
+      <View>
+        <TouchableOpacity onPress={onPressTeacher}>
+          <Text style={styles.title}>강사</Text>
+        </TouchableOpacity>
+      </View>
+      {pressTeacher === true ? 
+      <>
+        <DrawerItem
+          label="▪️ 강사 정보 관리"
+          onPress={()=>{
+            props.navigation.navigate('TeacherManageClass')
+          }}
+        />
+        <DrawerItem 
+          label="▪️ 강의별 출결 현황(강사)"
+          onPress={()=>{
+            props.navigation.navigate('TeacherAttendClass')
+          }}
+        /></>
+      :<></>}
+
       </DrawerContentScrollView>
       <Text style={{ fontSize: 16, textAlign: 'center', color: 'grey' }}>
         더존HERE
@@ -37,22 +129,18 @@ const CustomSidebarMenu = (props) => {
 };
 
 const styles = StyleSheet.create({
-  sideMenuProfileIcon: {
-    resizeMode: 'center',
-    width: 100,
-    height: 100,
-    borderRadius: 100 / 2,
+  headerImage: {
+    width: 70,
+    height: 70,
     alignSelf: 'center',
+    marginTop: 30
   },
-  iconStyle: {
-    width: 15,
-    height: 15,
-    marginHorizontal: 5,
-  },
-  customItem: {
+  title: {
     padding: 16,
     flexDirection: 'row',
     alignItems: 'center',
+    fontWeight: 'bold',
+    color: '#505050',
   },
 });
 
