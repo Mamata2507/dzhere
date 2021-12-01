bf43fd145fc3bdb23795f29a9ac2d1fb189f20b9
ryeongeunkim <ryeongeun.gim@gmail.com>
2021-12-01 17:12:12 +0900

    21.12.01(수) 주석삭제

--- dzhere-user-front/src/lib/api/myInfo/myInfo.js
+++ dzhere-user-front/src/lib/api/myInfo/myInfo.js
@@ -6,9 +6,6 @@ export const getEmail = phone =>
 export const updateEmail = ({phone, newEmail}) => 
     client.post(`/api/updateEmail/${phone}/${newEmail}`);
 
-// export const checkPw = ({currentPassword, phone}) => 
-//     client.post('/api/checkPw', {u_pw: currentPassword, u_phone: phone});
-
 // 비밀번호 중복체크
 export const checkPw = ({currentPassword, phone}) => {
     return client
