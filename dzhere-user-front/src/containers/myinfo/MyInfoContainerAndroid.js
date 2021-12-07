0a70cb09f29b4720050149541d75d521e92ff120
ryeongeunkim <ryeongeun.gim@gmail.com>
2021-12-07 18:34:00 +0900

    myinfo 수정

--- dzhere-user-front/src/containers/myinfo/MyInfoContainerAndroid.js
+++ dzhere-user-front/src/containers/myinfo/MyInfoContainerAndroid.js
@@ -1,16 +1,16 @@
-import { MyInfoAndroid } from "../../components/myinfo/MyInfoAndroid";
-import React, { useState, useEffect, useRef } from "react";
-import { useSelector } from "react-redux";
-import { Alert, Platform } from "react-native";
-import { apiLogout } from "../../lib/api/auth/auth";
-import { useDispatch } from "react-redux";
-import { logout } from "../../modules/auth/auth";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import client from "../../lib/api/client";
-import { useNavigation } from "@react-navigation/native";
-import Constants from "expo-constants";
-import * as Notifications from "expo-notifications";
-import { getClassTime } from "../../lib/api/myInfo/myInfo";
+import { MyInfoAndroid } from '../../components/myinfo/MyInfoAndroid'
+import React, { useState, useEffect, useRef } from 'react';
+import { useSelector } from 'react-redux';
+import { Alert, Platform } from 'react-native';
+import { apiLogout } from '../../lib/api/auth/auth'
+import { useDispatch } from 'react-redux';
+import { logout } from '../../modules/auth/auth'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import client from '../../lib/api/client';
+import { useNavigation } from '@react-navigation/native'
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import { getClassTime } from '../../lib/api/myInfo/myInfo';
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -21,13 +21,14 @@ Notifications.setNotificationHandler({
 });
 
 const MyInfoContainerAndroid = () => {
+
   const navigation = useNavigation();
 
   const dispatch = useDispatch();
 
   const phone = useSelector(({ auth }) => auth.userInfo.userPhone);
 
-  const [expoPushToken, setExpoPushToken] = useState("");
+  const [expoPushToken, setExpoPushToken] = useState('');
   const [notification, setNotification] = useState(false);
   const [notifyStatus, setNotifyState] = useState(false); // 알람 수신 상태
   const notificationListener = useRef();
@@ -35,37 +36,37 @@ const MyInfoContainerAndroid = () => {
 
   // 푸시 알람 기능
   function onNotify() {
-    setNotifyState(!notifyStatus);
+    setNotifyState(!notifyStatus)
   }
 
   // 알람 수신 상태 변경할 때 마다 실행
   useEffect(() => {
     // 푸시 알람 ON
-    if (notifyStatus === true) {
+    if(notifyStatus === true){
       async function schedulePushNotification() {
-        console.log("<<<<<<<< [ 푸시 알람 ON ] >>>>>>>>>>>");
+        console.log('<<<<<<<< [ 푸시 알람 ON ] >>>>>>>>>>>');
         const ct = await getClassTime(phone); // 학생의 수업 시간 가져오기
         let ct_start_hour = ct.ct_start_hour; // 수업 시작 시간
         let ct_start_minute = ct.ct_start_minute; // 수업 시작 10분 전
         let ct_end_hour = ct.ct_end_hour; // 수업 시작 시간
         let ct_end_minute = ct.ct_end_minute; // 수업 시작 10분 전
         let day_temp = ct.ct_day; // 수업 요일
-        let ct_day = day_temp.split("");
+        let ct_day = day_temp.split('');
         for (let i = 0; i < ct_day.length; i++) {
           switch (ct_day[i]) {
-            case "월":
+            case '월':
               ct_day[i] = 2;
               break;
-            case "화":
+            case '화':
               ct_day[i] = 3;
               break;
-            case "수":
+            case '수':
               ct_day[i] = 4;
               break;
-            case "목":
+            case '목':
               ct_day[i] = 5;
               break;
-            case "금":
+            case '금':
               ct_day[i] = 6;
               break;
             default:
@@ -106,39 +107,33 @@ const MyInfoContainerAndroid = () => {
           },
         });
       }
-      schedulePushNotification();
     }
+    schedulePushNotification();
+  } 
     // 푸시 알람 OFF
-    if (notifyStatus === false) {
+    if(notifyStatus === false) {
       async function deleteNotificationChannel() {
         console.log('<<<<<<<< [ 푸시 알람 OFF ] >>>>>>>>>>>');
         await Notifications.deleteNotificationChannelAsync('start_check');
         await Notifications.deleteNotificationChannelAsync('end_check');
-
       }
       deleteNotificationChannel();
     }
   }, [notifyStatus]);
 
   useEffect(() => {
-    registerForPushNotificationsAsync().then((token) =>
-      setExpoPushToken(token)
-    );
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
 
-    notificationListener.current =
-      Notifications.addNotificationReceivedListener((notification) => {
-        setNotification(notification);
-      });
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
 
-    responseListener.current =
-      Notifications.addNotificationResponseReceivedListener((response) => {
-        console.log(response);
-      });
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
 
     return () => {
-      Notifications.removeNotificationSubscription(
-        notificationListener.current
-      );
+      Notifications.removeNotificationSubscription(notificationListener.current);
       Notifications.removeNotificationSubscription(responseListener.current);
     };
   }, []);
@@ -146,37 +141,36 @@ const MyInfoContainerAndroid = () => {
   async function registerForPushNotificationsAsync() {
     let token;
     if (Constants.isDevice) {
-      const { status: existingStatus } =
-        await Notifications.getPermissionsAsync();
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
       let finalStatus = existingStatus;
-      if (existingStatus !== "granted") {
+      if (existingStatus !== 'granted') {
         const { status } = await Notifications.requestPermissionsAsync();
         finalStatus = status;
       }
-      if (finalStatus !== "granted") {
-        alert("Failed to get push token for push notification!");
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
         return;
       }
       token = (await Notifications.getExpoPushTokenAsync()).data;
       console.log(token);
     } else {
-      alert("Must use physical device for Push Notifications");
+      alert('Must use physical device for Push Notifications');
     }
-
-    if (Platform.OS === "android") {
-      Notifications.setNotificationChannelAsync("default", {
-        name: "default",
+  
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
         importance: Notifications.AndroidImportance.MAX,
         vibrationPattern: [0, 250, 250, 250],
-        lightColor: "#FF231F7C",
+        lightColor: '#FF231F7C',
       });
     }
     return token;
   }
 
   // 로그아웃
-  function onPress() {
-    apiLogout()
+  function onPress(){
+      apiLogout()
       .then(async (res) => {
         if (res.result) {
           console.log("result : ", res.message);
@@ -209,15 +203,15 @@ const MyInfoContainerAndroid = () => {
       .catch((e) => {
         console.log("apiLogout.catch - e:", e);
       });
-  }
+   } 
 
   return (
-    <MyInfoAndroid
-      phone={phone}
-      onPress={onPress} // 로그아웃
-      onNotify={onNotify} // 푸시알람
-      notifyStatus={notifyStatus} // 푸시 알람 수신 상태
-    />
+      <MyInfoAndroid
+          phone={phone}
+          onPress={onPress}           // 로그아웃
+          onNotify={onNotify}         // 푸시알람
+          notifyStatus={notifyStatus} // 푸시 알람 수신 상태
+      />
   );
 };
 
